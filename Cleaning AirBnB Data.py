29b456658e8e1e2e8ab93cbd2099ebf967265492
Joncwmusic <64911952+Joncwmusic@users.noreply.github.com>
2020-11-04 11:03:12 -0500

    Update Cleaning AirBnB Data.py

--- Cleaning AirBnB Data.py
+++ Cleaning AirBnB Data.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# from wordcloud import WordCloud, STOPWORDS
-# import folium
 
 # import and read in data
 
@@ -13,14 +11,16 @@ airbnb_file = 'AB_US_2020.csv'
 df_airbnb = pd.read_csv(airbnb_file, low_memory=False)
 
 # clean the data by removing id, host_id, host_name, neighborhood_group, neighborhood
+# these are all object datatypes and are largely irrelevant
 
 df_airbnb_grouped = df_airbnb.groupby(['city'])
-df_airbnb_numeric = df_airbnb.drop(['id', 'host_id', 'host_name', 'neighbourhood_group', 'neighbourhood', 'city', 'last_review'], axis=1)
-dummy_dum = pd.get_dummies(df_airbb_drop['room_type'])
+df_airbnb_useful = df_airbnb.drop(['id', 'host_id', 'host_name', 'neighbourhood_group', 'neighbourhood', 'last_review'], axis=1)
+df_airbnb_drop = df_airbnb.drop(['id', 'host_id', 'host_name', 'neighbourhood_group', 'neighbourhood', 'city', 'last_review'], axis=1)
+dummy_dum = pd.get_dummies(df_airbnb_useful['room_type'])
 
 # create another table with dummy values for room type
 
-df_airbnb_withdummy = pd.concat([df_airbnb_drop, dummy_dum], axis = 1, sort = False)
-df_airbnb_withdummy.drop(['name', 'room_type'], inplace = True, axis = 1)
-df_bnb_withdummy.to_csv('All_Numerical.csv')
+df_airbnb_withdummy = pd.concat([df_airbnb_useful, dummy_dum], axis = 1, sort = False)
+df_airbnb_numeric = df_airbnb_withdummy.drop(['name', 'room_type'], axis = 1)
+df_airbnb_numeric.to_csv('All_Numerical.csv')
 
